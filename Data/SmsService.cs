f5d180a4dc2effd39da9c34ea30e6281650695ef
slorello89 <slorello89@gmail.com>
2020-09-08 14:04:26 -0400

    first commit

---
+++ Data/SmsService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nexmo.Api.Messaging;
+using Nexmo.Api.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerHelloWorld.Data
+{
+    public class SmsService
+    {
+        private readonly IConfiguration _config;
+        public SmsService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SendSmsResponse SendSms(string to, string from, string text)
+        {
+            var apiKey = _config["VONAGE_API_KEY"];
+            var apiSecret = _config["VONAGE_API_SECRET"];            
+            var creds = Credentials.FromApiKeyAndSecret(apiKey, apiSecret);
+            var client = new SmsClient(creds);
+            var request = new SendSmsRequest
+            {
+                To = to,
+                From = from,
+                Text = text
+            };
+            return client.SendAnSms(request);
+        }
+    }
+}
---
+++ Data/WhatsAppService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Nexmo.Api.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerHelloWorld.Data
+{
+    public class WhatsAppService
+    {
+        private readonly IConfiguration _config;
+
+        public WhatsAppService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string SendWhatsAppMessage(string toNum, string fromNum, string text){
+            var appId = _config["APP_ID"];
+            var privateKey = _config["privateKeyPath"];
+            var content = new { type = "text", text };
+            var message = new { content };
+            var to = new { type = "whatsapp", number = toNum };
+            var from = new { type = "whatsapp", number = fromNum };
+            var creds = Credentials.FromAppIdAndPrivateKeyPath(appId, privateKey);
+            var uri = new Uri("https://messages-sandbox.nexmo.com/v0.1/messages");
+            var request = new { to, from, message };
+            var response = ApiRequest.DoRequestWithJsonContent<JObject>
+                ("POST", uri, request, ApiRequest.AuthType.Bearer, creds);
+            return response["message_uuid"].ToString();
+        }
+    }
+}
